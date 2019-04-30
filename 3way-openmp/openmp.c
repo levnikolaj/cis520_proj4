92da191a62f6dc1d694bc8b996d278c148093c81
levnikolaj <levnikolaj@gmail.com>
2019-04-29 19:52:21 -0500

    Added shell script and edited openmp.c

--- 3way-openmp/openmp.c
+++ 3way-openmp/openmp.c
@@ -7,11 +7,13 @@
 #include "sys/types.h"
 #include "sys/sysinfo.h"
 
-//****************************************************************
 
+#define PATH_TO_WIKI_DUMP				"/homes/levnikolaj/Proj4_520/wiki_dump.txt"
+#define MAX_CHARS_READ 					10000000
 /* Dynamically allocated array for storing substrings.
  */
 char **longestSubstring;
+void *findLongestSubstring();
 
 /* Stucture used to calculate virtual and physical memory.
  */
@@ -61,7 +63,7 @@ void GetProcessMemory(processMem_t* processMem)
 
 //************************************************************
 
-int main(int argc, char *arg[])
+int main()
 {
 	// to compile use(gcc -o openmp_comp -fopenmp openmp.c)
 	struct timeval t1, t2;
@@ -69,18 +71,20 @@ int main(int argc, char *arg[])
 	processMem_t myMem;
 	double elapsedTime;
 
-	numOfThreads = atoi(argv[1]); // argv[0] is the program name, argv[1] is the first argument
+	//numOfThreads = atoi(argv[1]); // argv[0] is the program name, argv[1] is the first argument, should be number of threads
 
-	omp_set_num_threads(numOfThreads);
+	//omp_set_num_threads(numOfThreads);
 
 	gettimeofday(&t1, NULL);
 
+	/*
 	#pragma omp parallel
 	{
-		/* void *functionName(omp_get_thread_num(), numOfThreads);
-		 *
-		 */
+		 void *functionName(omp_get_thread_num(), numOfThreads);
 	}
+  */
+
+	findLongestSubstring();
 
 	gettimeofday(&t2, NULL);
 	GetProcessMemory(&myMem);
@@ -91,4 +95,20 @@ int main(int argc, char *arg[])
 	printf("Memory: vMem, %u KB, pMem, %u KB\n", myMem.virtualMem, myMem.physicalMem);
 	printf("DATA, %f\n", elapsedTime);
 
+}
+
+void *findLongestSubstring()
+{
+	FILE *fd;
+	char firstLine[MAX_CHARS_READ];
+	char secondLine[MAX_CHARS_READ];
+	fd = fopen("/homes/levnikolaj/Proj4_520/wiki_dump.txt", "r");
+
+	fgets(firstLine, MAX_CHARS_READ, fd);
+	fgets(secondLine, MAX_CHARS_READ, fd);
+
+	printf("Line 1, %s\n", firstLine);
+	printf("Line 2, %s\n", secondLine);
+
+	fclose(fd);
 }
---
+++ 3way-openmp/shell_script_openmp.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/homes/levnikolaj/Proj4_520/3way-openmp/openmp_comp
