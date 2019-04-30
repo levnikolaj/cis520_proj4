a45e68b5f577b153b2734eab75ce225c6851885b
Samuel Moylan <smoylan22@WIN2.CS.KSU.EDU>
2019-04-29 20:01:27 -0500

    added algorithm for longest common substring

--- 3way-openmp/openmp.c
+++ 3way-openmp/openmp.c
@@ -112,3 +112,60 @@ void *findLongestSubstring()
 
 	fclose(fd);
 }
+
+void algorithm()
+{
+	char s1[] = "afsaldfkjlkshfachodybanksaslkdfjasfochodychodychody";
+	char s2[] = "asdfkllkjchodychodychodybankssfasdfkljsafklsa;dfuask;ldfk";
+	int s1_len = strlen(s1);
+	int s2_len = strlen(s2);
+	int table[s1_len + 1][s2_len + 1];
+
+	//initialize outside of table to zeros
+	for(int i = 0; i <= s1_len; i++)
+		table[i][0] = 0;
+	for(int i = 0; i <= s2_len; i++)
+		table[0][i] = 0;
+
+	int max = 0;
+	int col;
+	int val;
+
+	//set rest of table to correct values
+	//get max value of table and store associated column
+	for(int i = 1; i <= s1_len; i++)
+	{
+		for(int j = 1; j <= s2_len; j++)
+		{
+			if(s1[i-1] == s2[j-1])
+			{
+				val = table[i-1][j-1] + 1;
+				table[i][j] = val;
+				if(val > max)
+				{
+					max = val;
+					col = j;
+				}
+			}
+			else
+				table[i][j] = 0;
+		}
+	}
+
+	//create longest substring based on index of max value
+	if(max > 0)
+	{
+		char substr[max];
+		for(int i = 1; i <= max; i++)
+		{
+			substr[max-i] = s2[col-i];
+		}
+
+		printf("longest common substring: ");
+		for(int i = 0; i < max; i++)
+			printf("%c", substr[i]);
+		printf("\n");
+	}
+	else
+		printf("No common substring.\n");
+}
