d432a007dded79760ca4af60d558ea789b672f45
Mitchell Slavens <mslavens@WIN2.CS.KSU.EDU>
2019-05-01 21:27:14 -0500

    fixed base case

--- base_case/beocat_base.c
+++ base_case/beocat_base.c
@@ -2,53 +2,62 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include "sys/types.h"
+#include "sys/sysinfo.h"
+#include <sys/time.h>
 
 void algorithm();
+#define NUM_WIKI_LINES 2 //Should be 1000000
 
 void main()
 {
-  int i, j;
-  FILE * fd = fopen("/homes/dan/625/wiki_dump.txt", "r");
+  int i, j, p;
+  //FILE * fd = fopen("/homes/dan/625/wiki_dump.txt", "r");
+  FILE * fd = fopen("test.txt", "r");
   char *buffer = NULL;
   size_t n = 0;
-  char **wiki_dump = (char **) malloc(1000000 * sizeof(char *));
-  for(i = 0; i < 1000000; i++)
+  char **longestCommonSubstring = (char**) malloc((NUM_WIKI_LINES-1) * sizeof(char*));
+  char **wiki_dump = (char **) malloc(NUM_WIKI_LINES * sizeof(char *));
+  for(i = 0; i < NUM_WIKI_LINES; i++)
   {
     size_t line_length = getline(&buffer, &n, fd);
     wiki_dump[i] = (char*)malloc((line_length) * sizeof(char));
     memcpy(wiki_dump[i], buffer, sizeof(char) * line_length);
     wiki_dump[i][line_length-2] = 0;
   }
-  for(i = 0; i < 50; i++)
+  fclose(fd);
+  //Run Algorithm
+  p = 0;
+  for(i = 0; i < 1; i++, p++)
   {
-    for(j = 0; wiki_dump[i][j] != 0; j++)
-    {
-      printf("%c", wiki_dump[i][j]);
-    }
-    printf("\n");
+    algorithm(wiki_dump[i], wiki_dump[i+1], longestCommonSubstring, p);
   }
-  //algorithm();
+
+  //Print results
+  for(i = 0; i < NUM_WIKI_LINES-1; i++) // i < NUM_WIKI_LINES - 1
+	{
+		printf("Lines%d-%d: ", i, i+1);
+		if(longestCommonSubstring[i] != NULL)
+		{
+			printf("%s\n", longestCommonSubstring[i]);
+		}
+		else
+		{
+			printf("None found\n");
+		}
+	}
+  free(wiki_dump);
 }
 
-void algorithm()
+void algorithm(char *s1, char *s2, char ** resultsArray, int p)
 {
   int i, j;
-	char s1[] = "afsaldfkjlkshfachodybanksaslkdfjasfochodychodychody";
-	char s2[] = "asdfkllkjchodychodychodybankssfasdfkljsafklsa;dfuask;ldfk";
 	int s1_len = strlen(s1);
 	int s2_len = strlen(s2);
   //int table[s1_len + 1][s2_len + 1];
   int **table = (int **)malloc((s1_len + 1) * sizeof(int *));
   for(i = 0; i < s1_len+1; i++)
     table[i] = (int *)malloc((s2_len + 1) * sizeof(int));
-  /*int count = 0;
-  for(int i = 0; i < s1_len; i++)
-    for(int j = 0; j < s2_len; j++)
-    table[i][j] = ++count;
-  for(int i = 0; i < s1_len; i++)
-    for(int j= 0; j < s2_len; j++)
-      printf("%d", table[i][j]);*/
-
 
 	//initialize outside of table to zeros
 	for(i = 0; i <= s1_len; i++)
@@ -82,20 +91,16 @@ void algorithm()
 	}
 
 	//create longest substring based on index of max value
+  char substr[max];
 	if(max > 0)
 	{
-		char substr[max];
-		for(i = 1; i <= max; i++)
-		{
-			substr[max-i] = s2[col-i];
-		}
-
-		printf("longest common substring: ");
-		for(i = 0; i < max; i++)
-			printf("%c", substr[i]);
-		printf("\n");
+    for(i = 1; i <= max; i++)
+    {
+      substr[max-i] = s2[col-i];
+      resultsArray[p] = (char *) malloc((max) * sizeof(char));
+			memcpy(resultsArray[p], substr, sizeof(char) * max);
+			resultsArray[p][max] = 0;
+    }
 	}
-	else
-		printf("No common substring.\n");
 	free(table);
 }
